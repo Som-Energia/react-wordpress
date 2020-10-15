75a4373ec4b63826c3ba2ce380ef8a0b68065070
David Riera <david.riera@somenergia.coop>
2020-10-15 11:10:24 +0200

    add only entry point files

--- includes/enqueue.php
+++ includes/enqueue.php
@@ -12,28 +12,34 @@ add_action( 'init', function() {
 
   add_action( 'wp_enqueue_scripts', function() {
 
-    $asset_manifest = json_decode( file_get_contents( SOMRW_ASSET_MANIFEST ), true )['entrypoints'];
+    $asset_manifest = json_decode( file_get_contents( SOMRW_ASSET_MANIFEST ), true )['files'];
+    $entrypoints = json_decode( file_get_contents( SOMRW_ASSET_MANIFEST ), true )['entrypoints'];
 
     if ( isset( $asset_manifest[ 'main.css' ] ) ) {
       wp_enqueue_style( 'somrw', get_site_url() . SOMRW_STATIC . $asset_manifest[ 'main.css' ] );
     }
 
-    wp_enqueue_script( 'somrw-runtime', get_site_url() . SOMRW_STATIC . $asset_manifest[ 'runtime-main.js' ], array(), null, true );
+    if ( isset( $asset_manifest[ 'runtime-main.js' ] ) ) {
+      wp_enqueue_script( 'somrw-runtime', get_site_url() . SOMRW_STATIC . $asset_manifest[ 'runtime-main.js' ], array(), null, true );
 
-    wp_enqueue_script( 'somrw-main', get_site_url() . SOMRW_STATIC . $asset_manifest[ 'main.js' ], array('somrw-runtime'), null, true );
+      wp_enqueue_script( 'somrw-main', get_site_url() . SOMRW_STATIC . $asset_manifest[ 'main.js' ], array('somrw-runtime'), null, true );
+    }
 
     foreach ( $asset_manifest as $key => $value ) {
-      if ( preg_match( '@static/js/(.*)\.chunk\.js@', $key, $matches ) ) {
-        if ( $matches && is_array( $matches ) && count( $matches ) === 2 ) {
-          $name = "somrw-" . preg_replace( '/[^A-Za-z0-9_]/', '-', $matches[1] );
-          wp_enqueue_script( $name, get_site_url() . SOMRW_STATIC . $value, array( 'somrw-main' ), null, true );
-        }
-      }
 
-      if ( preg_match( '@static/css/(.*)\.chunk\.css@', $key, $matches ) ) {
-        if ( $matches && is_array( $matches ) && count( $matches ) == 2 ) {
-          $name = "somrw-" . preg_replace( '/[^A-Za-z0-9_]/', '-', $matches[1] );
-          wp_enqueue_style( $name, get_site_url() . SOMRW_STATIC . $value, array( 'somrw' ), null );
+      if ( in_array( $value, $entrypoints ) ) {
+        if ( preg_match( '@static/js/(.*)\.chunk\.js@', $key, $matches ) ) {
+          if ( $matches && is_array( $matches ) && count( $matches ) === 2 ) {
+            $name = "somrw-" . preg_replace( '/[^A-Za-z0-9_]/', '-', $matches[1] );
+            wp_enqueue_script( $name, get_site_url() . SOMRW_STATIC . $value, array( 'somrw-main' ), null, true );
+          }
+        }
+
+        if ( preg_match( '@static/css/(.*)\.chunk\.css@', $key, $matches ) ) {
+          if ( $matches && is_array( $matches ) && count( $matches ) == 2 ) {
+            $name = "somrw-" . preg_replace( '/[^A-Za-z0-9_]/', '-', $matches[1] );
+            wp_enqueue_style( $name, get_site_url() . SOMRW_STATIC . $value, array( 'somrw' ), null );
+          }
         }
       }
     }
