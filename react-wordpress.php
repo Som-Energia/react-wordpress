37594bb25a43e591a9d58144b777eb036a8e33d6
David Riera <david.riera@somenergia.coop>
2020-10-15 10:21:44 +0200

    changed widget for webforms

--- react-wordpress.php
+++ react-wordpress.php
@@ -6,11 +6,11 @@
 
 defined( 'ABSPATH' ) or die( 'Direct script access disallowed.' );
 
-define( 'SOMRW_WIDGET_PATH', plugin_dir_path( __FILE__ ) . '/widget' );
+define( 'SOMRW_WIDGET_PATH', plugin_dir_path( __FILE__ ) . '/webforms' );
 define( 'SOMRW_ASSET_MANIFEST', SOMRW_WIDGET_PATH . '/build/asset-manifest.json' );
 define( 'SOMRW_INCLUDES', plugin_dir_path( __FILE__ ) . '/includes' );
 
-define( 'SOMRW_STATIC', '/wp-content/plugins/react-wordpress/widget/build');
+define( 'SOMRW_STATIC', '/wp-content/plugins/react-wordpress/webforms/build');
 
 require_once( SOMRW_INCLUDES . '/enqueue.php' );
 require_once( SOMRW_INCLUDES . '/shortcode.php' );
