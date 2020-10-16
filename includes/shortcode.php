4f5d729c781a6c7f08a7edab0701cf656331f2d3
David Riera <david.riera@somenergia.coop>
2020-10-16 12:14:10 +0200

    fixed script global info include

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -7,16 +7,16 @@ add_shortcode( 'som_rw_webforms', function( $atts ) {
   $default_atts = array();
   $args = shortcode_atts( $default_atts, $atts );
 
-  $output = <<<XML
-    <script type="text/javascript">
+  $output = "
+    <script type=\"text/javascript\">
       // webforms config
       window.config = {
         API_BASE_URL: 'https://testapiv2.somenergia.coop:4433/',
       }
     </script>
 
-    <div id="root"></div>
-  XML;
+    <div id=\"root\"></div>
+  ";
 
   return $output;
 });
