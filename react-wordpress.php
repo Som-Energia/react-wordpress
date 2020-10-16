c17890a8eb59d3d6ccdc93ce293cc89149ad0429
David Riera <david.riera@somenergia.coop>
2020-10-16 10:17:37 +0200

    removed static path

--- react-wordpress.php
+++ react-wordpress.php
@@ -10,7 +10,5 @@ define( 'SOMRW_WIDGET_PATH', plugin_dir_path( __FILE__ ) . '/webforms' );
 define( 'SOMRW_ASSET_MANIFEST', SOMRW_WIDGET_PATH . '/build/asset-manifest.json' );
 define( 'SOMRW_INCLUDES', plugin_dir_path( __FILE__ ) . '/includes' );
 
-define( 'SOMRW_STATIC', '/wp-content/plugins/react-wordpress/webforms/build');
-
 require_once( SOMRW_INCLUDES . '/enqueue.php' );
 require_once( SOMRW_INCLUDES . '/shortcode.php' );
